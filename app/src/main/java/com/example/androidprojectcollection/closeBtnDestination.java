7ef3359e754ba95210e2ba78ce00f449de2343ec
LAB11_USER ON WS40 <L11X13W40@citulab.net>
2024-02-28 14:14:07 +0800

    yawa

---
+++ app/src/main/java/com/example/androidprojectcollection/closeBtnDestination.java
@@ -0,0 +1,14 @@
+package com.example.androidprojectcollection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class closeBtnDestination extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_close_btn_destination);
+    }
+}
